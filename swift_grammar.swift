4d7d41c83503002ffb808b9a9928ab5e7706df09
CheolheeLee0 <wks0968@gmail.com>
2023-01-11 21:59:17 +0900

    feat: swift_grammar

---
+++ swift_grammar.swift
@@ -0,0 +1,366 @@
+
+// 1. let, var
+let constant: String = "차후에 변경이 불가능한 상수 let"
+var variable: String = "차후에 변경이 가능한 변수 var"
+
+variable = "변수는 이렇게 차후에 다른 값을 할당할 수 있지만"
+// constant = "상수는 차후에 값을 변경할 수 없습니다" // 오류발생
+
+
+// 2. let: 선언 후 할당
+let sum: Int
+let inputA: Int = 100
+let inputB: Int = 200
+
+// 선언 후 첫 할당
+sum = inputA + inputB
+
+// sum = 1 // 그 이후에는 다시 값을 바꿀 수 없습니다, 오류발생
+
+// 변수도 물론 차후에 할당하는 것이 가능합니다
+var nickName: String
+
+nickName = "yagom"
+
+// 변수는 차후에 다시 다른 값을 할당해도 문제가 없지요
+nickName = "야곰"
+
+
+// 3. Bool
+var someBool: Bool = true
+someBool = false
+// someBool = 0 // 컴파일 오류발생
+// someBool = 1 // 컴파일 오류발생
+
+
+// 4. Int
+var someInt: Int = -100
+// someInt = 100.1 // 컴파일 오류발생
+
+
+// 5. UInt
+var someUInt: UInt = 100
+// someUInt = -100 // 컴파일 오류발생
+// someUInt = someInt // 컴파일 오류발생
+
+// 6. Float
+var someFloat: Float = 3.14
+someFloat = 3
+
+
+// 7. Double
+var someDouble: Double = 3.14
+someDouble = 3
+// someDouble = someFloat // 컴파일 오류발생
+
+
+// 8. Character, String
+var someCharacter: Character = "🇰🇷"
+someCharacter = "😄"
+someCharacter = "가"
+someCharacter = "A"
+// someCharacter = "하하하" // 컴파일 오류발생
+print(someCharacter)
+
+var someString: String = "하하하 😄 "
+someString = someString + "웃으면 복이와요"
+print(someString)
+
+// someString = someCharacter // 컴파일 오류발생
+
+someString = """
+여러줄 문자열을
+사용할 수 있습니다.
+첫 줄에 겹따옴표 세 개,
+마지막 줄에 겹따옴표 세 개를
+사용하면 됩니다.
+"""
+
+someString = """
+겹따옴표 세 개인 줄(첫줄, 끝줄)에서
+줄 바꿈을 하지 않으면 오류가 발생합니다.
+"""
+
+/*
+someString = """오류발생
+오류발생"""
+*/
+
+
+// 9. Any
+var someAny: Any = 100
+someAny = "어떤 타입도 수용 가능합니다"
+someAny = 123.12
+let someDouble: Double = someAny  // 컴파일 오류발생
+
+
+// 10. AnyObject
+class SomeClass {}
+var someAnyObject: AnyObject = SomeClass()
+someAnyObject = 123.12    // 컴파일 오류발생
+
+
+// 11. nil
+someAny = nil         // 컴파일 오류발생
+someAnyObject = nil   // 컴파일 오류발생
+
+
+// 12. Array
+// 빈 Int Array 생성
+var integers: Array<Int> = Array<Int>()
+
+// 같은 표현
+// var integers: Array<Int> = [Int]()
+// var integers: Array<Int> = []
+// var integers: [Int] = Array<Int>()
+// var integers: [Int] = [Int]()
+// var integers: [Int] = []
+// var integers = [Int]()
+
+integers.append(1)
+integers.append(100)
+
+// Int 타입이 아니므로 Array에 추가할 수 없습니다
+//integers.append(101.1)
+
+print(integers)	// [1, 100]
+
+// 멤버 포함 여부 확인
+print(integers.contains(100)) // true
+print(integers.contains(99)) // false
+
+// 멤버 교체
+integers[0] = 99
+
+// 멤버 삭제
+integers.remove(at: 0)
+integers.removeLast()
+integers.removeAll()
+
+// 멤버 수 확인
+print(integers.count)
+
+// 인덱스를 벗어나 접근하려면 익셉션 런타임 오류발생
+//integers[0]
+
+let immutableArray = [1, 2, 3]
+
+// 수정이 불가능한 Array이므로 멤버를 추가하거나 삭제할 수 없습니다
+//immutableArray.append(4)
+//immutableArray.removeAll()
+
+
+// 13. Dictionary
+// Key가 String 타입이고 Value가 Any인 빈 Dictionary 생성
+var anyDictionary: Dictionary<String, Any> = [String: Any]()
+
+// 같은 표현
+// var anyDictionary: Dictionary <String, Any> = Dictionary<String, Any>()
+// var anyDictionary: Dictionary <String, Any> = [:]
+// var anyDictionary: [String: Any] = Dictionary<String, Any>()
+// var anyDictionary: [String: Any] = [String: Any]()
+// var anyDictionary: [String: Any] = [:]
+// var anyDictionary = [String: Any]()
+
+// 키에 해당하는 값 할당
+anyDictionary["someKey"] = "value"
+anyDictionary["anotherKey"] = 100
+
+print(anyDictionary) // ["someKey": "value", "anotherKey": 100]
+
+// 키에 해당하는 값 변경
+anyDictionary["someKey"] = "dictionary"
+print(anyDictionary) ["someKey": "dictionary", "anotherKey": 100]
+
+// 키에 해당하는 값 제거
+anyDictionary.removeValue(forKey: "anotherKey")
+anyDictionary["someKey"] = nil
+print(anyDictionary)
+
+let emptyDictionary: [String: String] = [:]
+let initalizedDictionary: [String: String] = ["name": "yagom", "gender": "male"]
+
+// 불변 Dictionary이므로 값 변경 불가
+//emptyDictionary["key"] = "value"
+
+// 14. Set
+// 빈 Int Set 생성
+var integerSet: Set<Int> = Set<Int>()
+integerSet.insert(1)
+integerSet.insert(100)
+integerSet.insert(99)
+integerSet.insert(99)
+integerSet.insert(99)
+
+print(integerSet) // [100, 99, 1]
+print(integerSet.contains(1)) // true
+print(integerSet.contains(2)) // false
+
+integerSet.remove(100)
+integerSet.removeFirst()
+
+print(integerSet.count) // 1
+
+// Set는 집합 연산에 꽤 유용합니다
+let setA: Set<Int> = [1, 2, 3, 4, 5]
+let setB: Set<Int> = [3, 4, 5, 6, 7]
+
+// 합집합
+let union: Set<Int> = setA.union(setB)
+print(union) // [2, 4, 5, 6, 7, 3, 1]
+
+// 합집합 오름차순 정렬
+let sortedUnion: [Int] = union.sorted()
+print(sortedUnion) // [1, 2, 3, 4, 5, 6, 7]
+
+// 교집합
+let intersection: Set<Int> = setA.intersection(setB)
+print(intersection) // [5, 3, 4]
+
+// 차집합
+let subtracting: Set<Int> = setA.subtracting(setB)
+print(subtracting) // [2, 1]
+
+
+// 15. 함수
+func sum(a: Int, b: Int) -> Int {
+    return a + b
+}
+func printMyName(name: String) -> Void {
+    print(name)
+}
+// 반환 값이 없는 경우, 반환 타입(Void)을 생략해 줄 수 있습니다
+func printYourName(name: String) {
+    print(name)
+}
+func 함수이름() {
+    /* 함수 구현부 */
+    return
+}
+sum(a: 3, b: 5) // 8
+
+printMyName(name: "yagom") // yagom
+
+printYourName(name: "hana") // hana
+
+maximumIntegerValue() // Int의 최댓값
+
+hello() // hello
+
+bye() // bye
+
+func greeting(friend: String, me: String = "yagom") {
+    print("Hello \(friend)! I'm \(me)")
+}
+
+// 매개변수 기본값을 가지는 매개변수는 호출시 생략할 수 있습니다
+greeting(friend: "hana") // Hello hana! I'm yagom
+greeting(friend: "john", me: "eric") // Hello john! I'm eric
+
+// 16. 전달인자 레이블
+// 함수 내부에서 전달인자를 사용할 때에는 매개변수 이름을 사용합니다
+func greeting(to friend: String, from me: String) {
+    print("Hello \(friend)! I'm \(me)")
+}
+
+// 함수를 호출할 때에는 전달인자 레이블을 사용해야 합니다
+greeting(to: "hana", from: "yagom") // Hello hana! I'm yagom
+
+// 17. 가변 매개변수
+func sayHelloToFriends(me: String, friends: String...) -> String {
+    return "Hello \(friends)! I'm \(me)!"
+}
+print(sayHelloToFriends(me: "yagom", friends: "hana", "eric", "wing"))
+// Hello ["hana", "eric", "wing"]! I'm yagom!
+
+print(sayHelloToFriends(me: "yagom"))
+// Hello []! I'm yagom!
+
+
+// 18. 일급객체, 함수타입
+var someFunction: (String, String) -> Void = greeting(to:from:)
+someFunction("eric", "yagom") // Hello eric! I'm yagom
+
+someFunction = greeting(friend:me:)
+someFunction("eric", "yagom") // Hello eric! I'm yagom
+
+
+// 타입이 다른 함수는 할당할 수 없습니다 - 컴파일 오류 발생
+//someFunction = sayHelloToFriends(me: friends:)
+
+
+func runAnother(function: (String, String) -> Void) {
+    function("jenny", "mike")
+}
+
+// Hello jenny! I'm mike
+runAnother(function: greeting(friend:me:))
+
+// Hello jenny! I'm mike
+runAnother(function: someFunction)
+
+
+// 19. if-else
+let someInteger = 100
+
+if someInteger < 100 {
+    print("100 미만")
+} else if someInteger > 100 {
+    print("100 초과")
+} else {
+    print("100")
+} // 100
+
+
+// 20. switch
+// 범위 연산자를 활용하면 더욱 쉽고 유용합니다
+switch someInteger {
+case 0:
+    print("zero")
+case 1..<100:
+    print("1~99")
+case 100:
+    print("100")
+case 101...Int.max:
+    print("over 100")
+default:
+    print("unknown")
+} // 100
+
+// 정수 외의 대부분의 기본 타입을 사용할 수 있습니다
+switch "yagom" {
+case "jake":
+    print("jake")
+case "mina":
+    print("mina")
+case "yagom":
+    print("yagom!!")
+default:
+    print("unknown")
+} // yagom!!
+
+
+// 21. for-in
+var integers = [1, 2, 3]
+let people = ["yagom": 10, "eric": 15, "mike": 12]
+
+for integer in integers {
+    print(integer)
+}
+
+// Dictionary의 item은 key와 value로 구성된 튜플 타입입니다
+for (name, age) in people {
+    print("\(name): \(age)")
+}
+
+
+// 22. while
+while integers.count > 1 {
+    integers.removeLast()
+}
+
+
+// 23. repeat-while
+repeat {
+    integers.removeLast()
+} while integers.count > 0
