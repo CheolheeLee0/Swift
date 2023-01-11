7e40c6411d4823ad0a1be11edff22d9926bbc9c5
CheolheeLee0 <wks0968@gmail.com>
2023-01-11 21:26:44 +0900

    feat: swift project

---
+++ Instagram/Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by 이철희 on 2023/01/11.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
